b70dcd0504945310d274b1b0e43e4fe9b0547290
Kofi Ramos Amoussou <karsoft92@gmail.com>
2019-10-09 21:36:38 +0000

    added book cards to index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,54 +37,64 @@
         <section class="w-3/4 flex flex-wrap mx-2 mb-8">
           <section class="px-2 max-w-md  flex  w-full  md:w-1/2 lg:w-1/3 mb-3">
             <section class="border">
-            <img src="" alt="">
-            <section class="font-bold">Book 1</section>
+            <img src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" class="object-cover rounded-lg shadow-xl" alt="">
+          <section class="p-2">
+              <section class="font-bold">Book 2</section>
             <p class="text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor at, laborum! Maxime quibusdam aut corporis id similique aspernatur voluptatibus autem perspiciatis maiores et officia eaque magni vel libero, veniam quos! Dolore ut alias fuga atque libero, fugit error recusandae accusantium!
             </p>
+          </section>
           </section>
         </section>
         <!--  -->
         <section class="px-2 max-w-md  flex  w-full  md:w-1/2 lg:w-1/3 mb-3">
             <section class="border">
-            <img src="" alt="">
-            <section class="font-bold">Book 1</section>
+             <img src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" class="object-cover rounded-lg shadow-xl" alt="">
+          <section class="p-2">
+              <section class="font-bold">Book 2</section>
             <p class="text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor at, laborum! Maxime quibusdam aut corporis id similique aspernatur voluptatibus autem perspiciatis maiores et officia eaque magni vel libero, veniam quos! Dolore ut alias fuga atque libero, fugit error recusandae accusantium!
             </p>
+          </section>
           </section>
         </section>
         <!--  -->
 
          <section class="px-2 max-w-md  flex  w-full  md:w-1/2 lg:w-1/3 mb-3">
             <section class="border">
-            <img src="" alt="">
-            <section class="font-bold">Book 1</section>
+             <img src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" class="object-cover rounded-lg shadow-xl" alt="">
+          <section class="p-2">
+              <section class="font-bold">Book 2</section>
             <p class="text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor at, laborum! Maxime quibusdam aut corporis id similique aspernatur voluptatibus autem perspiciatis maiores et officia eaque magni vel libero, veniam quos! Dolore ut alias fuga atque libero, fugit error recusandae accusantium!
             </p>
+          </section>
           </section>
         </section>
         <!--  -->
 
          <section class="px-2 max-w-md  flex  w-full  md:w-1/2 lg:w-1/3 mb-3">
             <section class="border">
-            <img src="" alt="">
-            <section class="font-bold">Book 1</section>
+             <img src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" class="object-cover rounded-lg shadow-xl" alt="">
+          <section class="p-2">
+              <section class="font-bold">Book 2</section>
             <p class="text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor at, laborum! Maxime quibusdam aut corporis id similique aspernatur voluptatibus autem perspiciatis maiores et officia eaque magni vel libero, veniam quos! Dolore ut alias fuga atque libero, fugit error recusandae accusantium!
             </p>
+          </section>
           </section>
         </section>
         <!--  -->
 
          <section class="px-2 max-w-md  flex  w-full  md:w-1/2 lg:w-1/3 mb-3">
             <section class="border">
-            <img src="" alt="">
-            <section class="font-bold">Book 1</section>
+             <img src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" class="object-cover rounded-lg shadow-xl" alt="">
+          <section class="p-2">
+              <section class="font-bold">Book 2</section>
             <p class="text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor at, laborum! Maxime quibusdam aut corporis id similique aspernatur voluptatibus autem perspiciatis maiores et officia eaque magni vel libero, veniam quos! Dolore ut alias fuga atque libero, fugit error recusandae accusantium!
             </p>
+          </section>
           </section>
         </section>
         <!--  -->
